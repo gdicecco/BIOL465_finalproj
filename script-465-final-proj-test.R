192e325832971e6d7069381cb6bf231fb50ef84d
gdicecco <gdicecco@live.unc.edu>
2017-11-09 11:11:45 -0500

    minor edits

--- script-465-final-proj-test.R
+++ script-465-final-proj-test.R
@@ -8,10 +8,13 @@ require(dplyr)
 checklist <- read.csv("/Users/gracedicecco/Desktop/git/LTER_birdabund_seasonal/BirdLife_Checklist_V_9.1.csv", header = TRUE)
 checklist.subs <- checklist[,c(4,13)]
 checklist.subs$Common_name <- tolower(checklist.subs$Common_name)
+#remove dashes, replace with spaces
+checklist.subs$Common_name <- gsub("-", " ", checklist.subs$Common_name)
 
 #Konza LTER
 konza <- read.csv("/Users/gracedicecco/Desktop/git/LTER_birdabund_seasonal/CBP011_0.csv")
 konza$COMMONNAME <- tolower(konza$COMMONNAME)
+konza$COMMONNAME <- gsub("-", " ", konza$COMMONNAME)
 #January, April, June, October
 konza_id <- left_join(konza, checklist.subs, by = c("COMMONNAME" = "Common_name"))
 
@@ -19,6 +22,7 @@ konza_id <- left_join(konza, checklist.subs, by = c("COMMONNAME" = "Common_name"
 parkriver <- read.csv("/Users/gracedicecco/Desktop/git/LTER_birdabund_seasonal/MON-EX-PRNWR-Volunteer-Birds.csv", header = TRUE)
 #Every two months, use January, April, June, October
 parkriver$Common.Name <- tolower(parkriver$Common.Name)
+parkriver$Common.Name <- gsub("-", " ", parkriver$Common.Name)
 parkriver_id <- left_join(parkriver, checklist.subs, by = c("Common.Name" = "Common_name"))
 
 #Sevilleta LTER
@@ -29,6 +33,7 @@ sev_table_col <- separate(sev_table, 1, into = c("AOU", "CommonName"), sep = "  
 sev_table_col$AOU <- as.factor(sev_table_col$AOU)
 sev_join <- left_join(sev, sev_table_col, by = c("species_number" = "AOU")) #join common name to AOU
 sev_join$CommonName <- tolower(sev_join$CommonName)
+sev_join$CommonName <- gsub("-", " ", sev_join$CommonName)
 sev_id <- left_join(sev_join, checklist.subs, by = c("CommonName" = "Common_name")) #join ID number by common name
 
 #Luquillo LTER
@@ -44,17 +49,18 @@ luquillo_long <- gather(luquillo, Species, Abundance, 11:53)
 luquillo_long_naomit <- na.omit(luquillo_long) #remove NAs
 luquillo_commonnames <- left_join(luquillo_long_naomit, luquillo_sppnames, by = c("Species" = "Code")) #add common names
 luquillo_commonnames$CommonName <- tolower(luquillo_commonnames$CommonName)
+luquillo_commonnames$CommonName <- gsub("-", " ", luquillo_commonnames$CommonName)
 luquillo_id <- left_join(luquillo_commonnames, checklist.subs, by = c("CommonName" = "Common_name"))
 
 ###Check that all species were paired with an ID from checklist
-spp_parkriver <- unique(parkriver_id[,c(6,9)])
-spp_sev <- unique(sev_id[,8:9])
-spp_konza <- unique(konza_id[,c(12,21)])
-spp_luquillo <- unique(luquillo_id[,15:16])
+spp_parkriver <- unique(parkriver_id[,c(6,9)]) #102 spp
+spp_sev <- unique(sev_id[,8:9]) #83
+spp_konza <- unique(konza_id[,c(12,21)]) #137
+spp_luquillo <- unique(luquillo_id[,15:16]) #41
 #NAs: mainly where there are old common names being used
 
 #number of NAs: 55 total across sites
 sev.nas <- spp_sev[is.na(spp_sev$SISRecID),] #15 species
 parkriver.nas <- spp_parkriver[is.na(spp_parkriver$SISRecID),] #9 species
 konza.nas <- spp_konza[is.na(spp_konza$SISRecID),] #18 species
-luquillo.nas <- spp_luquillo[is.na(spp_luquillo$SISRecID),] #13 species
+luquillo.nas <- spp_luquillo[is.na(spp_luquillo$SISRecID),] #11 species
